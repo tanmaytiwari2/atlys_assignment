826cf137993e217819661b618e0cadc4da974a0c
tanmaytwr01 <tanmaytwr01@gmail.com>
2024-07-08 00:31:00 +0530

    add webpack optimizations

--- src/router.js
+++ src/router.js
@@ -1,7 +1,12 @@
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import RootLayout from "./components/organisms/RootLayout";
-import Feeds from "./pages/Feeds";
+// import Feeds from "./pages/Feeds";
 import Login from "./pages/Login/index";
+import { lazy } from "react";
+
+const Feeds = lazy(() =>
+  import(/* webpackChunkName: "Feeds" */ "./pages/Feeds")
+);
 
 export const router = createBrowserRouter([
   {
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,59 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   output: {
-    path: path.join(__dirname, "/dist"), // the bundle output path
-    filename: "bundle.js", // the name of the bundle
+    uniqueName: "atlys",
+    publicPath: "/",
+    path: path.join(__dirname, "/dist"),
+    filename: `static/js/[name].[chunkhash:8].js`,
+    chunkFilename: `static/js/[name].[chunkhash:8].js`,
+    assetModuleFilename: `images/[path][name].[contenthash:8][ext]`,
+    crossOriginLoading: "anonymous",
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html", // to import index.html file inside index.js
+      template: "public/index.html",
+      favicon: "public/favicon.ico",
     }),
+    new MiniCssExtractPlugin(),
   ],
+  performance: {
+    maxAssetSize: 250000,
+    maxEntrypointSize: 1000000,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      maxSize: 500000,
+      cacheGroups: {
+        common: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -5,
+          reuseExistingChunk: true,
+          chunks: "initial",
+          name: "common_app",
+          minSize: 0,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+        defaultVendors: false,
+        reactPackage: {
+          test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/,
+          name: "vendor_react",
+          chunks: "all",
+          priority: 10,
+        },
+      },
+    },
+    sideEffects: true,
+  },
+
   devServer: {
     port: 3000, // you can change the port
     historyApiFallback: true,
@@ -25,13 +68,13 @@ module.exports = {
         },
       },
       {
-        test: /\.(sa|sc|c)ss$/, // styles files
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
         loader: "url-loader",
         options: { limit: false },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
